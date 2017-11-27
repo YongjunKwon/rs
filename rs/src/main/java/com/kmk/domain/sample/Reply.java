6a64e7d65edbe04bd2f33bbe9cc6afb04d4e3e53
kyoungsam <artist_mks@naver.com>
2017-11-27 20:23:17 +0900

    리플달기적용

---
+++ rs/src/main/java/com/kmk/domain/sample/Reply.java
@@ -0,0 +1,22 @@
+package com.kmk.domain.sample;
+
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class Reply {
+	
+	private int reply_seq;
+	private int seq;
+	private String content;
+	private String del_flag;
+	private String user_id;
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date reg_dtime;
+	private String yyyymmdd;
+	
+	private String nick_nm;
+}
